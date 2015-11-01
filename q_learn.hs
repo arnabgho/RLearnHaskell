c372842139c74adbad09c2393560a5873d9f6245
Arnab Ghosh <arnabgho@iitk.ac.in>
2015-11-01 19:16:08 +0530

    Q Learning Code Complete Testing Remains

--- q_learn.hs
+++ q_learn.hs
@@ -1,51 +1,59 @@
 import Data.Map (Map)
 import qualified Data.Map as Map
 
--- fe for feedback 
--- st for state
--- ac for action
--- qval for value of q
+-- fe for feedback Fractional
+-- st for state Int
+-- ac for action Int
+-- qval for value of q Fractional
 
 inf=1e9
+neg_inf=(-inf)
 learningRate=0.1
 discountFactor=0.1
+eps=1e-5
 
 q= Map.empty
 
-getQ::st->ac->qval
+getQ::Int->Int->Double
+--getQ::(Integral a,Double b)=>a-> a ->b
 getQ state action=q Map.! (state,action)
 
 greater::(Ord a)=>a->a->a
 greater x y = if x>y then x else y
 
-getMaxActionVal::(Ord qval)=> st-> [ac]->qval
+getMaxActionVal::Int-> [Int]->Double
+--getMaxActionVal:: (Integral a,Double b)=>a-> [a]->b
 getMaxActionVal newState actions=let x=map (getQ newState) actions
-									in foldl greater -inf x  
+									in foldl greater neg_inf x  
 
-giveFeedback::fe->st->[ac]->st->ac->Bool
-giveFeedback feedback newState actions lastState lastAction=let maxim=0
-										maxim=getMaxActionVal actions
-										lastQ=q Map.!(lastState,lastAction)
-										newQ=lastQ+learningRate*(feedback + discountFactor* maxim - lastQ)
-										Map.insert (lastState,lastAction) newQ q 
-										in True 
 
-checkZero::st->[ac]->ac
+
+checkZero::Int->[Int]->Int
 checkZero state [] = -1
-checkZero state (action:rest) = if q Map.! (state,action) ==0 then action
-								else checkZero state rest
+checkZero state (action:rest) = if abs(q Map.! (state,action) )<eps then action
+								else checkZero state rest									
 
-findAction::st->[ac]->qval->ac
-findAction state [] val=-1
-findAction state (action:rest) val=if q Map.! action == val then action
-									else findAction state rest val
 
-getAction::st->[ac]->ac
-getAction state actions=if x \= -1 then x
-						else y 
-						where
-						x=checkZero state actions 
-						z=map (getQ state) actions
-						yval=foldl greater -inf z
-						y=findAction state actions yval
+findAction::Int->[Int]->Double->Int
+findAction state [] val=(-1)
+findAction state (action:rest) val=if abs(q Map.! action - val)<eps then action
+									else findAction state rest val		
 
+
+giveFeedback::Double->Int->[Int]->Int->Int->Double
+giveFeedback feedback newState actions lastState lastAction=let{
+  	maxim=getMaxActionVal newState actions;
+    lastQ=q Map.! (lastState,lastAction);
+	newQ=lastQ+learningRate*(feedback + discountFactor* maxim - lastQ);
+	res=Map.insert (lastState,lastAction) newQ q; 	
+	} in newQ														
+
+
+getAction::Int->[Int]->Int
+getAction state actions=if x /= -1 then x else y 
+	where{ 
+		  x=checkZero state actions; 
+		  z=map (getQ state) actions;
+		  yval=foldl greater neg_inf z;
+		  y=findAction state actions yval;
+		}  
