78bb64f39068321e2c60c9f7772f760c5b282c1b
Arnab Ghosh <arnabgho@iitk.ac.in>
2015-11-10 22:57:57 +0530

    SARSA Int mess cleared

--- src/SARSA_eps.hs
+++ src/SARSA_eps.hs
@@ -2,7 +2,7 @@ module SARSA where
 import System.Random
 import qualified Data.Map as Map
 
-type Table = Map.Map (Int, Int) Double
+type Table = Map.Map (Integer, Integer) Double
 
 inf = 1e9
 negInf = -inf
@@ -10,31 +10,31 @@ learningRate = 0.1
 discountFactor = 0.1
 epsilon = 1e-2
 
-initializeState :: Int -> [Int] -> Table -> Table
+initializeState :: Integer -> [Integer] -> Table -> Table
 initializeState s [] tab = tab
 initializeState s (a:as) tab = Map.insert (s, a) 0 $ initializeState s as tab
 
-initializeStates :: [Int] -> [Int] -> Table -> Table
+initializeStates :: [Integer] -> [Integer] -> Table -> Table
 initializeStates [] as tab = tab
 initializeStates (s:rs) as tab = initializeStates rs as $ initializeState s as tab
                        
 
-getValAtIndex::[Int]->Int->Int
+getValAtIndex::[Integer]->Integer->Integer
 getValAtIndex [] _ = -1
 getValAtIndex (x:xs) 1 = x
 getValAtIndex (x:xs) n = getValAtIndex xs n-1
 
-getRandomAction::[Int]->StdGen->Int
+getRandomAction::[Integer]->StdGen->Integer
 getRandomAction actions g=randAction 
 	where{
-			numActions = length actions ;
-			(randomActionIndex,g')=randomR (1,numActions) g ::(Int,StdGen);
+			numActions = toInteger $ length actions ;
+			(randomActionIndex,g')=randomR (1,numActions) g ::(Integer,StdGen);
 			randAction=getValAtIndex actions randomActionIndex;
 		}
 
 -- TODO epsilon greedy => random
 -- TODO maybe type
-getBestAction :: Int -> [Int] -> Table -> Int
+getBestAction :: Integer -> [Integer] -> Table -> Integer
 getBestAction s [] q = -1
 getBestAction s (act:acts) q = bestAction 
 			 where thisActionVal = q Map.! (s, act)
@@ -42,7 +42,7 @@ getBestAction s (act:acts) q = bestAction
                                remVal = q Map.! (s, remActionBest)
                                bestAction = if thisActionVal > remVal then act else remActionBest
 
-getAction::Int->[Int]->Table->StdGen->(Int,StdGen)
+getAction::Integer->[Integer]->Table->StdGen->(Integer,StdGen)
 getAction state actions q g=if prob<epsilon then (randAction,g') else (bestAction,g') 
 	where{ 
 			(prob,g')=randomR (0,1.00) g :: (Double,StdGen) ;
@@ -50,7 +50,7 @@ getAction state actions q g=if prob<epsilon then (randAction,g') else (bestActio
 			bestAction=getBestAction state actions q;
 		}  
 
-updateQ :: Double -> Int -> Int -> Int -> Int -> Table -> Table
+updateQ :: Double -> Integer -> Integer -> Integer -> Integer -> Table -> Table
 updateQ reward state action newState newAction q = Map.insert (state, action) newQVal q
 						 where thisQ = q Map.! (state, action)
 						       nextQ = q Map.! (newState, newAction) 
