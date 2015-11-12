49c64fff2634e69afe91ef839af99df711362334
Arpit Shrivastava <arp.shri@gmail.com>
2015-11-13 03:47:10 +0530

    compiling version of QLearn.hs

--- src/Learn/SARSA.hs
+++ src/Learn/SARSA.hs
@@ -7,6 +7,6 @@ import Debug.Trace
 
 updateQ :: (Show state, Show action, Ord state, Ord action) => Double -> state -> action -> state -> action -> Table state action -> Table state action
 updateQ reward state action newState newAction q = Table { table = Map.insert (state, action) newQVal (table q)}
-						 where thisQ = trace ("State: " ++ show(state) ++ "\nAction: " ++ show(action) ) (table q) Map.! (state, action)
-						       nextQ = trace ("State: " ++ show(newState) ++ "\nAction: " ++ show(newAction) ) (table q) Map.! (newState, newAction) 
-						       newQVal = thisQ + Helper.learningRate * ( reward + Helper.discountFactor * nextQ - thisQ )
+						 where thisQ = {- trace ("State: " ++ show(state) ++ "\nAction: " ++ show(action) ) -} table q Map.! (state, action)
+						       nextQ = {-trace ("State: " ++ show(newState) ++ "\nAction: " ++ show(newAction) ) -} table q Map.! (newState, newAction) 
+						       newQVal = thisQ + Helper.learningRate * ( reward + Helper.discountFactor * nextQ - thisQ ) 
